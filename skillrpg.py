668fdc257e2bc71e6075a6f0fcee0ffe975810c2
Roberto E. Riquelme Sáez <rers.92@gmail.com>
2023-01-07 23:36:38 -0300

    added basic things to the game

---
+++ skillrpg.py
@@ -0,0 +1,25 @@
+
+class Skill(object):
+    def __init__(self, name : str) -> None:
+        self.name = name
+        self.level = 0
+
+    def level_set(self, level : int) -> None:
+        self.level =  level
+
+    def level_up(self) -> None:
+        self.level += 1
+
+class Lvl_char (object):
+    def __init__(self) -> None:
+        self.exp = 0
+        self.lvl = 1
+        self.exp_next = 50
+    def gain_exp(self, exp : int):
+        self.exp += exp
+        if self.exp >= self.exp_next:
+            self.exp -= self.exp_next
+            self.lvl += 1
+            self.exp_next = (self.lvl *50 * 2** (self.lvl - 1) )
+    
+
