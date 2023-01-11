46374148adb7d80b04b30b7a36306853e1008559
Roberto E. Riquelme Sáez <rers.92@gmail.com>
2023-01-11 10:33:10 -0300

    draw testing rectangle

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 import pygame
 
+white = (255,255,255)
+grey =  (169,169,169)
+black = (  0,  0,  0)
+blue =  (  0,  0,200)
 pygame.init()
 screen = pygame.display.set_mode([500, 500])
 running = True
@@ -7,8 +11,9 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-    screen.fill((250, 250, 250))
-    pygame.draw.circle(screen, (0, 0, 255), (250, 250), 75)
+    screen.fill(grey)
+    #pygame.draw.circle(screen, (0, 0, 255), (250, 250), 75)
+    pygame.draw.rect(screen,blue,(50,50,100,100))
     pygame.display.flip()
 
 pygame.quit()
