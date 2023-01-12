f042bb5285a19a9122f5b2bbaa7ce19d3a66fca1
Roberto E. Riquelme Sáez <rers.92@gmail.com>
2023-01-12 15:00:59 -0300

    layout game, parametrized

--- main.py
+++ main.py
@@ -2,18 +2,25 @@ import pygame
 
 white = (255,255,255)
 grey =  (169,169,169)
+gray1 =(250,250,250)
+gray2 =(240,240,240)
+gray3 =(230,230,230)
 black = (  0,  0,  0)
 blue =  (  0,  0,200)
+square_draw = (150,150)
+resolution_display = [500,500]
 pygame.init()
-screen = pygame.display.set_mode([500, 500])
+screen = pygame.display.set_mode(resolution_display)
 running = True
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-    screen.fill(grey)
-    #pygame.draw.circle(screen, (0, 0, 255), (250, 250), 75)
-    pygame.draw.rect(screen,blue,(50,50,100,100))
+    screen.fill(gray1)
+
+    pygame.draw.rect(screen,grey,(0,0,square_draw[0],square_draw[1]))
+    pygame.draw.rect(screen,gray1,(square_draw[0],0,resolution_display[0]-square_draw[0],resolution_display[1]))
+    pygame.draw.rect(screen,gray2,(0,square_draw[1],square_draw[0],resolution_display[1]-square_draw[1]))
     pygame.display.flip()
 
 pygame.quit()
